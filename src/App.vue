fc8ae2b1b7115dd8f833dfdb8b53a87facc40733
giuliatognali <118864811+giuliatognali@users.noreply.github.com>
2023-03-31 15:26:47 +0200

    store app

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <script>
+import axios from 'axios';
+
+import { store } from './store';
 
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
@@ -7,7 +10,20 @@ export default {
   components: {
     AppHeader,
     AppMain
-  }
+  },
+    data() {
+        return {
+            store
+        }
+    },
+    created() {
+        axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php').then((response) => {
+            //console.log(response);
+            this.store.characters = response.data.data;
+            this.store.charactersFound = response.data.data.length;
+            
+        })
+    }
 }
 </script>
 
